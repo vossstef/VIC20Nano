e446bcce1df84b5680573368675f93a2bfb5326e
Stefan Voss <75468830+vossstef@users.noreply.github.com>
2025-04-13 12:55:58 +0200

    Xml keymap (#31)
    move xml and keyboard ammping into core

--- README.md
+++ README.md
@@ -24,12 +24,15 @@ This project relies on an external µC being connected to the Tang Nano 20K. You
 For the [M0S Dock BL616](https://wiki.sipeed.com/hardware/en/maixzero/m0s/m0s.html) there is a:
 
 * [Optional custom carrier board MiSTeryShield20k](https://github.com/harbaum/MiSTeryNano/tree/main/board/misteryshield20k/README.md)
-* [Optional case](https://github.com/prcoder-1/MiSTeryNano-Case)
-* [Dualshock Adapter / Cable](/board/misteryshield20k_ds2_adapter/misteryshield20k_ds2_adapter_cable.md)
+* [Optional case](https://github.com/harbaum/MiSTeryNano/blob/main/board/misteryshield20k/housing3D)
+* [Dualshock Adapter / Cable](https://github.com/vossstef/tang_nano_20k_c64/tree/main/board/misteryshield20k_ds2_adapter/misteryshield20k_ds2_adapter_cable.md)
 
 and for the Raspberry PiPico (RP2040 µC) there is a:
 
-* [Optional custom carrier board MiSTeryShield20k Raspberry PiPico](/board/misteryshield20k_rpipico/README.md)
+* [Optional custom carrier board MiSTeryShield20k Raspberry PiPico](https://github.com/vossstef/tang_nano_20k_c64/tree/main/board/misteryshield20k_rpipico/README.md)
+* [Dualshock Adapter / Cable](https://github.com/vossstef/tang_nano_20k_c64/tree/main/board/misteryshield20k_ds2_adapter/misteryshield20k_ds2_adapter_cable.md)
+* [Optional custom PMOD RP2040-Zero](https://github.com/vossstef/tang_nano_20k_c64/tree/main/board/pizero_pmod/README.md) for Primer / Mega Boards
+* Optional case (upcoming)
 
 Features:
 
--- src/misc/hid.v
+++ src/misc/hid.v
@@ -3,7 +3,7 @@
  
     hid (keyboard, mouse etc) interface to the IO MCU
 
-    c64 core specific variant of hid
+    VIC core specific variant of hid
   */
 
 module hid (
@@ -19,6 +19,7 @@ module hid (
   input  [5:0]    db9_port,
   output reg	  irq,
   input			  iack,
+
   // output HID data received from USB
   output reg [7:0] joystick0,
   output reg [7:0] joystick1,
@@ -59,7 +60,18 @@ reg [7:0] command;
 reg [7:0] device;   // used for joystick
 reg irq_enable;
 reg [5:0] db9_portD;
-     
+reg [5:0] db9_portD2;
+
+// translate incoming HID key codes into
+// VIC key matrix positions
+wire [2:0] kbd_row;
+wire [2:0] kbd_column;   
+keymap keymap (
+       .code   ( data_in[6:0] ),
+       .row    ( kbd_row      ),
+       .column ( kbd_column   )
+);  
+   
 // process mouse events
 always @(posedge clk) begin
    if(reset) begin
@@ -78,10 +90,12 @@ always @(posedge clk) begin
       keyboard[ 6] <= 8'hff; keyboard[ 7] <= 8'hff; 
 
    end else begin
+      db9_portD <= db9_port;
+      db9_portD2 <= db9_portD;
+      
       // monitor db9 port for changes and raise interrupt
       if(irq_enable) begin
-        db9_portD <= db9_port;
-        if(db9_portD != db9_port) begin
+        if(db9_portD2 != db9_portD) begin
             // irq_enable prevents further interrupts until
             // the db9 state has actually been read by the MCU
             irq <= 1'b1;
@@ -90,32 +104,33 @@ always @(posedge clk) begin
       end
 
       if(iack) irq <= 1'b0;      // iack clears interrupt
+
       mouse_strobe <=1'b0;
       joystick_strobe <=1'b0; 
       if(data_in_strobe) begin      
         if(data_in_start) begin
-            state <= 4'd1;
+            state <= 4'd0;
             command <= data_in;
-        end else if(state != 4'd0) begin
+        end else begin
             if(state != 4'd15) state <= state + 4'd1;
 	    
             // CMD 0: status data
             if(command == 8'd0) begin
-                // return some dummy data for now ...
-                if(state == 4'd1) data_out <= 8'h5c;
-                if(state == 4'd2) data_out <= 8'h42;
+                if(state == 4'd0) data_out <= 8'h01;
+                if(state == 4'd1) data_out <= 8'h00;
             end
 	   
             // CMD 1: keyboard data
             if(command == 8'd1) begin
-                if(state == 4'd1) keyboard[data_in[2:0]][data_in[5:3]] <= data_in[7]; 
+	       // kbd_column and kbd_row are derived from data_in
+               if(state == 4'd0) keyboard[kbd_row][kbd_column] <= data_in[7]; // row / colum !
             end
 	       
             // CMD 2: mouse data
             if(command == 8'd2) begin
-                if(state == 4'd1) mouse_btns <= data_in[1:0];
-                if(state == 4'd2) mouse_x <= data_in;
-                if(state == 4'd3) begin 
+                if(state == 4'd0) mouse_btns <= data_in[1:0];
+                if(state == 4'd1) mouse_x <= data_in;
+                if(state == 4'd2) begin 
                     mouse_y <= data_in; 
                     mouse_strobe <=1'b1; 
                 end
@@ -123,8 +138,8 @@ always @(posedge clk) begin
 
             // CMD 3: receive digital joystick data
             if(command == 8'd3) begin
-                if(state == 4'd1) device <= data_in;
-                if(state == 4'd2) begin
+                if(state == 4'd0) device <= data_in;
+                if(state == 4'd1) begin
                     if(device == 8'd0) joystick0 <= data_in;
                     if(device == 8'd1) joystick1 <= data_in;
                     if(device == 8'h80) begin // 0, 0, KP * button2, KP0 trigger, KP 8 up, KP 2 down, KP 4 left, KP 6 right
@@ -134,15 +149,15 @@ always @(posedge clk) begin
                         tape_play <= data_in[7];
                      end
                 end
-                if(state == 4'd3) begin
+                if(state == 4'd2) begin
                         if(device == 8'd0) joystick0ax <= data_in;
                         if(device == 8'd1) joystick1ax <= data_in;
                 end
-                if(state == 4'd4) begin
+                if(state == 4'd3) begin
                         if(device == 8'd0) joystick0ay <= data_in;
                         if(device == 8'd1) joystick1ay <= data_in;
                 end
-                if(state == 4'd5) begin
+                if(state == 4'd4) begin
                         if(device == 8'd0) extra_button0 <= data_in;
                         if(device == 8'd1) extra_button1 <= data_in;
                         joystick_strobe <= 1'b1;
@@ -151,8 +166,8 @@ always @(posedge clk) begin
 
             // CMD 4: send digital joystick data to MCU
             if(command == 8'd4) begin
-                if(state == 4'd1) irq_enable <= 1'b1;    // (re-)enable interrupt
-                data_out <= {2'b00, db9_port };               
+                if(state == 4'd0) irq_enable <= 1'b1;    // (re-)enable interrupt
+                data_out <= {2'b00, db9_portD };               
             end
         end
       end
